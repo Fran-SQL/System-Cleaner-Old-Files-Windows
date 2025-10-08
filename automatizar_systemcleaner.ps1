9a737ceb6373c714b108d2db7a54e1c8dad04b59
Fran-SQL <bestversion955@gmail.com>
2025-10-08 10:50:43 +0200

    Create automatizar_systemcleaner.ps1
    Script para programar la automatización de SystemCleaner.ps1

---
+++ automatizar_systemcleaner.ps1
@@ -0,0 +1,11 @@
+# --- PROGRAMAR AUTOMATIZACIÓN SystemCleaner.ps1 ---
+$taskName = "SystemCleaner_Automatico" # Nombre tarea
+$scriptPath = "C:\ruta\SystemCleaner\SystemCleaner.ps1" # Ruta script, indicar ruta donde esté SystemCleaner.ps1
+
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`"" # Cómo ejecutar script
+$trigger = New-ScheduledTaskTrigger -Daily -At 10:30am # Cuándo se ejecuta el script, ejemplo diariamente a las 10:30AM
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -RunLevel Highest # conceder privilegios más altos para poder limpiar carpetas del sistema como Temp
+
+Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -TaskName $taskName -Description "Limpieza diaria del sistema con PowerShell"
+
+Write-Host "Tarea programada '$taskName' creada correctamente."

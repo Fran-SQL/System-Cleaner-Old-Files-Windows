a038bf1ab6593ce4247518cc1a7675f41f64866d
Fran-SQL <bestversion955@gmail.com>
2025-10-08 11:02:34 +0200

    Update automatizar_systemcleaner.ps1
    Modificar $scriptPath para obtenerlo automaticamente

--- automatizar_systemcleaner.ps1
+++ automatizar_systemcleaner.ps1
@@ -1,6 +1,6 @@
 # --- PROGRAMAR AUTOMATIZACIÓN SystemCleaner.ps1 ---
 $taskName = "SystemCleaner_Automatico" # Nombre tarea
-$scriptPath = "C:\ruta\SystemCleaner\SystemCleaner.ps1" # Ruta script, indicar ruta donde esté SystemCleaner.ps1
+$scriptPath = Join-Path $PSScriptRoot "SystemCleaner.ps1" # Ruta donde está SystemCleaner.ps1
 
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-ExecutionPolicy Bypass -File `"$scriptPath`"" # Cómo ejecutar script
 $trigger = New-ScheduledTaskTrigger -Daily -At 10:30am # Cuándo se ejecuta el script, ejemplo diariamente a las 10:30AM
